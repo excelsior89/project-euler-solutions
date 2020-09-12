3fff49212a3c22af072f70e44585adeaec94fb2e
Paul-Robertson <66646154+paul-sm-nz@users.noreply.github.com>
2020-09-11 22:16:48 -0700

    Added problem 15. Replaced integer instances with 64 bit typedef in all problems. Removed trivial constructor declarations from problem source files, moved these to headers.

---
+++ problems/include/PeProblem15.h
@@ -0,0 +1,27 @@
+// Copyright 2020 Paul Robertson
+//
+// PeProblem15.h
+//
+// "Lattice paths"
+
+#pragma once
+
+#include "PeProblem.h"
+#include "PeUtilities.h"
+
+namespace pe {
+class PeProblem15: public PeProblem
+{
+public:
+	PeProblem15() {}
+	virtual ~PeProblem15() {}
+
+	virtual std::ostream &DisplayProblem(std::ostream &os = std::cout) override;
+	virtual std::ostream &DisplaySolution(std::ostream &os = std::cout) override;
+	virtual std::ostream &ProfileSolutions(int n_trials, std::ostream &os = std::cout) override;
+
+private:
+	static const int kProblemNumber = 15;
+};
+}; // namespace pe
+
